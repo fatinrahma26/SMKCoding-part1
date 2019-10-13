aa778c93e35226f31079bd8f6111ad33841eb65e
fatinrahma26 <fatinrahma137@gmail.com>
2019-10-13 10:25:40 +0700

    first commit

---
+++ app/src/main/java/com/example/e_catering/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.e_catering;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/e_catering/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.e_catering
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import jdk.nashorn.internal.runtime.ECMAException.getException
+import org.junit.experimental.results.ResultMatchers.isSuccessful
+import androidx.annotation.NonNull
+
+
+
